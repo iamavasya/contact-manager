abe95e3ec3e6b07c01a889c4681b1f0173448458
Rostyslav Mukha <snowyfl1cker@gmail.com>
2024-10-02 12:47:39 +0300

    Initial commit, dividing project into N-Layer pattern

---
+++ contact-manager/contact-manager.Infrastructure/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace contact_manager.Infrastructure.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public bool Married { get; set; }
+        public string Phone { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
